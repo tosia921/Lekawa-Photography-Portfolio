709c25a7c6bbf057fb6724af3c675ada17887fd9
Kamila Tarnożek <tomaszposiadala@gmail.com>
2021-06-24 13:16:03 +0100

    deployment changes

--- strapi-backend/config/env/production/database.js
+++ strapi-backend/config/env/production/database.js
@@ -1,26 +1,23 @@
-  
-const parse = require('pg-connection-string').parse
-const config = parse(process.env.DATABASE_URL)
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => ({
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'postgres',
-          host: congif.host,
-          port: congif.port,
-          database: congif.database,
-          username: congif.user,
-          password: congif.password,
-          ssl: {
-              rejectUnauthorized: false
-          }
-        },
-        options: {
-          
+module.exports = () => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
         },
       },
+      options: {},
     },
-  });
+  },
+});
--- strapi-backend/config/server.js
+++ strapi-backend/config/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env('STRAPI_URL')
-})
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ee6b206a3b2606a50c9bb1c337229f46'),
+    },
+  },
+});
