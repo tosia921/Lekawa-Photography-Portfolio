9a920e54b1b2c65ca3ac35e42f15dbb10c8e3bb4
Kamila Tarnożek <tomaszposiadala@gmail.com>
2021-06-24 11:28:32 +0100

    deployment settings

--- strapi-backend/config/database.js
+++ strapi-backend/config/database.js
@@ -1,15 +1,25 @@
+const parse = require('pg-connection-string').parse
+const config = parse(process.env.DATABASE_URL)
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: congif.host,
+          port: congif.port,
+          database: congif.database,
+          username: congif.user,
+          password: congif.password,
+          ssl: {
+              rejectUnauthorized: false
+          }
+        },
+        options: {
+          
+        },
       },
     },
-  },
-});
+  });
