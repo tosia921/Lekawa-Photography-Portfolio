1516edefe9141cfce054d88de21082f6590ecd4a
Kamila Tarnożek <tomaszposiadala@gmail.com>
2021-06-24 16:01:40 +0100

    deployment changes

--- strapi-backend/config/server.js
+++ strapi-backend/config/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env('STRAPI_URL')
-})
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('DO_SPACE_SECRET_KEY', 'MKV3F3BM2YNS5Z3PT4P4'),
+    },
+  },
+});
