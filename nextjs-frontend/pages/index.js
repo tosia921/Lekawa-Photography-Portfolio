8f1cbfd997ac5145b4841413cbb06e37e3ba3623
Tomasz Posiadala <tomaszposiadala@gmail.com>
2021-07-09 12:20:56 +0100

    meta tag changes 2

--- nextjs-frontend/pages/index.js
+++ nextjs-frontend/pages/index.js
@@ -15,6 +15,8 @@ import PublicationsPreview from '../components/PublicationsPreview';
 import BookingSection from '../components/BookingSection';
 // Media Queries
 import { device } from '../styles/Media';
+// meta image
+import metaImage from '../public/images/LekawaScreenshot.png';
 
 const StyledMain = styled.main`
     .ImageAlicja {
@@ -92,7 +94,7 @@ const Homepage = ({ imageGalleries, currLocale, publications }) => {
                     )}
                 />
                 <meta property="og:title" content="Tomasz Lekawa Photography" />
-                <meta property="og:image" content="https://www.lekawa-photography.co.uk" />
+                <meta property="og:image" content={metaImage} />
                 <meta
                     property="og:description"
                     content={t(
