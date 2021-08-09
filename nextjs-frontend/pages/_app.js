25f49172b2498a50d6dc5041665ab8beac21fbbd
Tomasz Posiadala <tomaszposiadala@gmail.com>
2021-08-09 22:23:39 +0100

    added google analytics

--- nextjs-frontend/pages/_app.js
+++ nextjs-frontend/pages/_app.js
@@ -3,10 +3,22 @@ import Layout from '../components/Layout';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
 import '../styles/styles.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-P7YKKLFH45" />
+            <Script strategy="lazyOnload">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+          
+                gtag('config', 'G-P7YKKLFH45');
+                `}
+            </Script>
+
             <GlobalStyles />
             <Typography />
             <Layout>
