b98fb55f2acd9d6d04e2bef2a22ab86bc8a59848
Tomasz Posiadala <tomaszposiadala@gmail.com>
2021-08-09 21:43:04 +0100

    installed and configured next-sitemap to generate robots.txt and sitemap.xml file in a postbuild script

---
+++ nextjs-frontend/next-sitemap-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    siteUrl: 'https://lekawa-photography.co.uk/',
+    generateRobotsTxt: true,
+    alternateRefs: [
+        {
+            href: 'https://lekawa-photography.co.uk/',
+            hreflang: 'en-gb',
+        },
+        {
+            href: 'https://lekawa-photography.co.uk/pl/',
+            hreflang: 'pl',
+        },
+    ],
+};
